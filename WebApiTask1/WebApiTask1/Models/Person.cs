9d2e6678a91f306d8c4aa9e3b98d7c6fce239a24
juhomontonen <juhop.montonen@gmail.com>
2019-01-13 19:50:37 +0200

    Database initialized

---
+++ WebApiTask1/WebApiTask1/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiTask1.Models
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            Phone = new HashSet<Phone>();
+        }
+
+        public long Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        public short? Age { get; set; }
+
+        [InverseProperty("Person")]
+        public virtual ICollection<Phone> Phone { get; set; }
+    }
+}
---
+++ WebApiTask1/WebApiTask1/Models/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiTask1.Models
+{
+    public partial class Phone
+    {
+        public long Id { get; set; }
+        public string Type { get; set; }
+        [StringLength(10)]
+        public string Number { get; set; }
+        public long? PersonId { get; set; }
+
+        [ForeignKey("PersonId")]
+        [InverseProperty("Phone")]
+        public virtual Person Person { get; set; }
+    }
+}
